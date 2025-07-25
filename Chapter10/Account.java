925e9711923bcbdabb3c7576c5c62d80f8edeacb
Jimbo <Jacobson8998@gmail.com>
2025-07-25 10:29:43 -0600

    Create Account.java

---
+++ Chapter10/Account.java
@@ -0,0 +1,62 @@
+import java.util.Date;
+
+public class Account {
+    private int id = 0;
+    private double balance = 0.0;
+    private double annualInterestRate = 0.0;
+    private Date dateCreated;
+
+    public Account() {
+        this.dateCreated = new Date();
+    }
+
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+        this.dateCreated = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public double getMonthlyInterestRate() {
+        return annualInterestRate / 12;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * (getMonthlyInterestRate() / 100);
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}

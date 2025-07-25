0ecc2ee9f5570db66e9f924e106236bf45bc4e4e
Jimbo <Jacobson8998@gmail.com>
2025-07-25 10:30:41 -0600

    Create MyInteger.java

---
+++ Chapter10/MyInteger.java
@@ -0,0 +1,71 @@
+public class MyInteger {
+    private int value;
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isEven() {
+        return value % 2 == 0;
+    }
+
+    public boolean isOdd() {
+        return value % 2 != 0;
+    }
+
+    public boolean isPrime() {
+        if (value <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(value); i++) {
+            if (value % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static boolean isOdd(int n) {
+        return n % 2 != 0;
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static boolean isEven(MyInteger n) {
+        return n.isEven();
+    }
+
+    public static boolean isOdd(MyInteger n) {
+        return n.isOdd();
+    }
+
+    public static boolean isPrime(MyInteger n) {
+        return n.isPrime();
+    }
+
+    public boolean equals(int n) {
+        return value == n;
+    }
+
+    public boolean equals(MyInteger n) {
+        return value == n.getValue();
+    }
+
+    public static int parseInt(char[] chars) {
+        return Integer.parseInt(new String(chars));
+    }
+
+    public static int parseInt(String str) {
+        return Integer.parseInt(str);
+    }
+}
